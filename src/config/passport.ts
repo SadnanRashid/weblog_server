ae1b9ca0a54add20d11e0ab32496ba65b1d3be2e
SadnanRashid <sadnan.rashid.07@gmail.com>
2024-05-13 00:26:10 +0600

    setup server

---
+++ src/config/passport.ts
@@ -0,0 +1,41 @@
+import {
+  Strategy as JwtStrategy,
+  ExtractJwt,
+  StrategyOptions,
+} from "passport-jwt";
+import config from "./config";
+import { tokenTypes } from "./tokens";
+import { User } from "../models";
+import { Request } from "express";
+
+interface Payload {
+  type: string;
+  sub: string;
+}
+
+const jwtOptions: StrategyOptions = {
+  secretOrKey: config.jwt.secret,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+};
+
+const jwtVerify = async (
+  payload: Payload,
+  done: (error: any, user?: any) => void
+) => {
+  try {
+    if (payload.type !== tokenTypes.ACCESS) {
+      throw new Error("Invalid token type");
+    }
+    const user = await User.findById(payload.sub);
+    if (!user) {
+      return done(null, false);
+    }
+    done(null, user);
+  } catch (error) {
+    done(error, false);
+  }
+};
+
+const jwtStrategy = new JwtStrategy(jwtOptions, jwtVerify);
+
+export { jwtStrategy };
