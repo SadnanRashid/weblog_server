c91de013fbdfccf4dda5a5e869bdbf24e3ffe9cd
SadnanRashid <sadnan.rashid.07@gmail.com>
2024-05-13 00:27:54 +0600

    setup server

---
+++ src/config/token.ts
@@ -0,0 +1,17 @@
+type tokenType = {
+  ACCESS: string;
+  REFRESH: string;
+  RESET_PASSWORD: string;
+  VERIFY_EMAIL: string;
+};
+
+const tokenTypes: tokenType = {
+  ACCESS: "access",
+  REFRESH: "refresh",
+  RESET_PASSWORD: "resetPassword",
+  VERIFY_EMAIL: "verifyEmail",
+};
+
+module.exports = {
+  tokenTypes,
+};
