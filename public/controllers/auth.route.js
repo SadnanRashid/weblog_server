bcc0cd9d0f3bc9d873245e23286e0de6fa8bbdb4
SadnanRashid <sadnan.rashid.07@gmail.com>
2024-05-22 15:15:49 +0600

    user auth

---
+++ public/controllers/auth.route.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const auth_controller_1 = require("./auth.controller");
+const authRouter = express_1.default.Router();
+authRouter.post("/register", auth_controller_1.authController.register);
+authRouter.post("/login", auth_controller_1.authController.login);
+authRouter.post("/logout", auth_controller_1.authController.logout);
+authRouter.post("/refresh-tokens", auth_controller_1.authController.refreshTokens);
+exports.default = authRouter;
--- public/routes/index.js
+++ public/routes/index.js
@@ -5,11 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const users_route_1 = __importDefault(require("./users.route"));
+const auth_route_1 = __importDefault(require("../controllers/auth.route"));
 const router = express_1.default.Router();
 const defaultRoutes = [
     {
         path: "/users",
         route: users_route_1.default,
+    },
+    {
+        path: "/auth",
+        route: auth_route_1.default,
     },
 ];
 defaultRoutes.forEach((route) => {
